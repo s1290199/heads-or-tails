4b4736a8dbef7f8c645cc2de680e2212bc54f1ef
s1290199 <s1290199@u-aizu.ac.jp>
2022-07-14 21:23:20 +0900

    User name added

--- HorT.java
+++ HorT.java
@@ -1,10 +1,17 @@
 import java.util.Random;
 import java.lang.Math;
+import java.util.Scanner;
 
 public class HorT {
     public static void main(String[] args){
         Random rand = new Random();
         int hc = 0, tc = 0;
+
+        System.out.println("Who are you?");
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        System.out.println("Hello,"+str+"!");
+
         
         System.out.println("Tossing a coin...");
         for(int i = 1 ; i < 4 ; i++){
